15837cc97d84db22089fbf005f4437baed7db5c2
vedos <vedo_spi@live.com>
2021-04-24 11:47:59 +0200

    sec

---
+++ MovieDB/Helper/DataSeeder.cs
@@ -0,0 +1,33 @@
+﻿using MovieDB.Models;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace MovieDB.Helper
+{
+    public class DataSeeder
+    {
+        public static void SeedMovies(MovieContext context)
+        {
+            if (!context.Movies.Any())
+            {
+                var movies = new List<Movie>
+            {
+                new Movie { Title = "Afghanistan" },
+                new Movie { Title = "Albania" },
+                new Movie { Title = "Algeria" },
+                new Movie { Title = "Andorra" },
+                new Movie { Title = "Angola" },
+                new Movie { Title = "Antigua and Barbuda" },
+                new Movie { Title = "Argentina" },
+                new Movie { Title = "Armenia" },
+                new Movie { Title = "Aruba" },
+                new Movie { Title = "Australia" },
+                new Movie { Title = "Austria" },
+                new Movie { Title = "Azerbaijan" }
+            };
+                context.AddRange(movies);
+                context.SaveChanges();
+            }
+        }
+    }
+}
--- MovieDB/Program.cs
+++ MovieDB/Program.cs
@@ -1,5 +1,7 @@
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using MovieDB.Helper;
 
 namespace MovieDB
 {
@@ -7,7 +9,16 @@ namespace MovieDB
     {
         public static void Main(string[] args)
         {
-            CreateHostBuilder(args).Build().Run();
+            var host = CreateHostBuilder(args)
+                .Build();
+
+            using (var scope = host.Services.CreateScope())
+            {
+                var dbContext = scope.ServiceProvider.GetRequiredService<MovieContext>();
+                DataSeeder.SeedMovies(dbContext);
+            }
+
+            host.Run();
         }
 
         public static IHostBuilder CreateHostBuilder(string[] args) =>
@@ -15,6 +26,13 @@ namespace MovieDB
                 .ConfigureWebHostDefaults(webBuilder =>
                 {
                     webBuilder.UseStartup<Startup>();
+
+                }).ConfigureServices((hostContext, services) =>
+                {
+                    services.Configure<HostOptions>(option =>
+                    {
+                        
+                    });
                 });
     }
 }
